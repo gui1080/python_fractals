6a5a906cf87b2e3efb33406df148acf708c45283
Guilherme Braga <guib545@aluno.unb.br>
2022-07-20 11:53:31 -0300

    eita

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+import yfinance as yahooFinance
+from datetime import datetime, timedelta
 
 from examples.mandel import mandelbrot
 
@@ -8,10 +11,19 @@ if __name__ == '__main__':
     
     print("Começo do script")
     
+    hoje = datetime.today().strftime('%Y-%m-%d')
+    ontem = datetime.today() - timedelta(days=1)
+    
+    ontem = ontem.strftime('%Y-%m-%d')
+    
+    #ibov = yf.Ticker("^BVSP")
+    ibovespa = yahooFinance.Ticker("EWZ")
+    
+    print(ibovespa.history(start=ontem, end=hoje))
+    
     output = mandelbrot(1000,1000,150)   
     
-    print(output)
+    #print(output)
     plt.imshow(output.T, cmap = "hot")
     plt.axis("off")
-    plt.show()
-    
+    plt.savefig("mandelbrot.png")
